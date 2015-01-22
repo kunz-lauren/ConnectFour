f848565d839e40bc70630a35218aaa131c0d9124
DielTenille <SuperMommy@Tenille>
2015-01-21 19:35:49 -0700

    Initial commit- Added instance variables, and functions, etc.

--- ConnectFour/src/connectfour/ConnectFour.java
+++ ConnectFour/src/connectfour/ConnectFour.java
@@ -5,17 +5,40 @@
  */
 package connectfour;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Keith
  */
 public class ConnectFour {
-
+    //Instance Variables
+    String name;
+    String instructions = "This is the game of Connect Four \n\n"
+            + "This is a two player game. \n"
+            + "The point of the game is to get four of your pieces \n"
+            + "in a row, either hortizonaly, vertically, or diagionally. \n"
+            + "Player take turns placing their pieces on a six by six board. \n"
+            + "The first person to connect four of their pieces in a row wins! \n"
+            + "Good Luck!";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ConnectFour myGame = new ConnectFour();
+        myGame.getName();
+        myGame.displayHelp();
+                
     }
-    
+    //Functions
+    public void getName() {
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter your name: ");
+        this.name = input.next();
+}
+    public void displayHelp() {
+        System.out.println("\nWelcome " + this.name + "\n");
+        System.out.println(this.instructions);
+        
+    }
 }
