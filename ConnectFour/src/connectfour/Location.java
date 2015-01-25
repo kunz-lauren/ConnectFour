207e79cd34c81002907ecd7240651525290bcee7
Garaix-Davy <dgaraix@gmail.com>
2015-01-24 18:15:01 -0800

    test.

--- ConnectFour/src/connectfour/Location.java
+++ ConnectFour/src/connectfour/Location.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package connectfour;
 
 /**
@@ -19,5 +15,5 @@ public class Location {
 
     public int getRowNumber() {
         return rowNumber;
-    }    
+    }
 }
