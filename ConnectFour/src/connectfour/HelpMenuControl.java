690e06f51c050f157bf5ff1f670f2f6f3f59e1ac
Garaix-Davy <dgaraix@gmail.com>
2015-02-03 18:15:49 -0800

    Added HelpMenuControl and HelpMenuView.

---
+++ ConnectFour/src/connectfour/HelpMenuControl.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connectfour;
+
+/**
+ *
+ * @author Davy
+ */
+public class HelpMenuControl {
+    
+    public HelpMenuControl() {
+        
+    } 
+
+    public void displayGameHelp() {
+        System.out.println();
+        displayHelpBorder();     
+        System.out.println( 
+                 "\tThe objective of the game is to be the first player to match four pieces "
+                + "\n\tvertically, horizontally or diagonally. Each player takes "
+                + "\n\tturns placing their marker in one of the locations on the "
+                + "\n\tboard. The first player to connect four is the winner."
+                ); 
+        displayHelpBorder();
+    }
+
+    public void displayPlayerHelp() {
+        System.out.println();
+        displayHelpBorder();     
+        System.out.println( 
+                "\tEach player takes turn by placing their piece "
+                + "\n\tin an unused location on the board."
+                ); 
+        displayHelpBorder();
+    }
+    
+    public void displayBoardHelp() {
+        System.out.println();
+        displayHelpBorder();             
+        System.out.println( 
+                "\tThe game board for Connect Four consists of a grid of "
+                + "\n\tlocations. Each player places their markers in the different locations "
+                + "\n\tof the board in an attempt to win the game while blocking the other player."
+                + "\n\tThe default board is 6 rows by 6 columns.");
+        displayHelpBorder();
+    }
+    
+    public void displayColumnHelp() {
+        System.out.println();
+        displayHelpBorder();     
+        System.out.println( 
+               "\tThere are 6 available columns. When a player selects "
+                + "\tthe column they want their marker to be in, "
+                + "\tThe game piece will drop to the lowest available slot."
+                );
+        displayHelpBorder();
+    }    
+        
+    public void displayMarkerHelp() {
+        System.out.println();
+        displayHelpBorder();     
+        System.out.println( 
+               "\tA marker is an object that occupies a location in the board "
+                + "\n\tThere are two different types of markers, one for each player."
+                ); 
+        displayHelpBorder();
+    }
+            
+    public void displayHelpBorder() {       
+        System.out.println(
+        "\t********************************************************************");
+    }
+
+    public void displayError() {       
+        System.out.println();
+        displayHelpBorder();                     
+        System.out.println("\tThis is an invalid selection. Please choose again.");
+        displayHelpBorder();             
+    }
+    
+}
---
+++ ConnectFour/src/connectfour/HelpMenuView.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connectfour;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Davy
+ */
+public class HelpMenuView {
+    
+
+    
+    private final static String[][] helpItems = {
+        {"G", "The game"},
+        {"P", "A regular player"},   
+        {"B", "The board"},
+        {"C", "A column"},
+        {"M", "A marker"},     
+        {"Q", "Quit Help"}        
+    };    
+    
+    // Create instance of the HelpMenuControl (action) class
+    private HelpMenuControl helpMenuControl = new HelpMenuControl();
+    
+    // default constructor
+    public HelpMenuView() {
+        
+    } 
+    
+    // display the help menu and get the end users input selection
+    public void getInput() {       
+              
+        String command;
+        
+        // if the program doesn run, this is what we should fix
+        Scanner input = new Scanner(System.in);
+        
+        do {
+            
+            this.display(); // display the menu
+            
+            // get commaned entered
+            command = input.nextLine();
+            command = command.trim().toUpperCase();
+            
+            switch (command) {
+                case "G":
+                    this.helpMenuControl.displayGameHelp();
+                    break;
+                    
+                case "P":
+                    this.helpMenuControl.displayPlayerHelp();
+                    break;                     
+                
+                case "B":
+                    this.helpMenuControl.displayBoardHelp();
+                    break;
+                    
+                case "C":
+                    this.helpMenuControl.displayColumnHelp();
+                    break;                   
+                    
+                case "M":
+                    this.helpMenuControl.displayMarkerHelp();
+                    break;
+                    
+                case "Q": 
+                    break;
+                    
+                default: 
+                    this.helpMenuControl.displayError();
+                    continue;
+            }
+        } while (!command.equals("Q"));  
+        
+         return;
+    }
+
+        // displays the help menu
+    public final void display() {
+        System.out.println("\n\t===============================================================");
+        System.out.println("\tEnter the letter associated with one of the following commands:");
+
+        for (int i = 0; i < HelpMenuView.helpItems.length; i++) {
+            System.out.println("\t   " + helpItems[i][0] + "\t" + helpItems[i][1]);
+        }
+        System.out.println("\t===============================================================\n");
+    }
+  
+}
