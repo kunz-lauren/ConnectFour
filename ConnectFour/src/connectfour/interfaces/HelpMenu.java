9e90da226dd9b613214476bab5df8b81bf72dfd1
Garaix-Davy <dgaraix@gmail.com>
2015-03-18 22:29:29 -0700

    Lesson 10 - Individual Assignment - French enums, interfaces, packages

---
+++ ConnectFour/src/connectfour/interfaces/HelpMenu.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connectfour.interfaces;
+
+/**
+ *
+ * @author Davy
+ */
+public interface HelpMenu {
+    
+    public void afficheAideJeu();
+    public void afficheAideJoueur();
+    public void afficheAidePlateau();
+    public void afficheAideColonne();
+    public void afficheAideJeton();
+    public void afficheAideBordureSupérieure();
+    public void afficheAideBordureInférieure();
+    public void afficheErreur();
+    
+}
