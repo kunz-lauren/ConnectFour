2196c8c7e705ff41eda7bc6396fd785f77ee8930
laurenkorpacz <laurenkorpacz@10.0.1.13>
2015-03-21 18:54:17 -0600

    Week 10 Individual Assignment

---
+++ ConnectFour/src/connectfour/menus/CheckAvailibiltyView.java
@@ -0,0 +1,25 @@
+package connectfour.menus;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Keoni AledoJr
+ */
+public class CheckAvailibiltyView {
+    
+    public void findAvailable(String[][] board, int column){
+        int actualCol = column*2-1;
+        if(board[1][actualCol].equals(" ")){
+            System.out.println("You can put a piece in this column.");
+        } else{
+            System.out.println("This column is full. Pick a different one.");
+        }
+    }
+    
+}
+
