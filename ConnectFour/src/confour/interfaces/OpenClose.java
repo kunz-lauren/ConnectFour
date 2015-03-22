cee1619dabebf760f1392511c1f1b4065e815333
laurenkorpacz <laurenkorpacz@10.0.1.13>
2015-03-21 18:41:17 -0600

    Week 10 Individual Assignment

---
+++ ConnectFour/src/confour/interfaces/OpenClose.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package confour.interfaces;
+
+/**
+ *
+ * @author laurenkorpacz
+ */
+public interface OpenClose {
+    
+  public void open ();
+  public void close ();
+}
