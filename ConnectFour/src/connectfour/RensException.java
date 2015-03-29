140210fa8345cc9f646b6d13e3a66c8981be095d
laurenkorpacz <laurenkorpacz@10.0.1.13>
2015-03-29 00:16:44 -0600

    Week 11 Individual Assignment

---
+++ ConnectFour/src/connectfour/RensException.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connectfour;
+import static connectfour.ConnectFour.input;
+import java.util.Scanner;
+
+/**
+ *
+ * @author laurenkorpacz
+ */
+public class RensException {
+  public static void main(String[] args) throws ManyExceptions {
+ 
+    try {
+      long data[] = new long[1000000000]; 
+    }
+    catch (Exception e) {
+      System.out.println(e);
+    }
+ 
+    finally {
+      System.out.println("finally block will execute always.");
+    }
+  }
+}
